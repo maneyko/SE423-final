528e458da8a6881bac5cf97aefadd7b96477caba
Mechatronics8 <coecsl@uiuc.com>
2019-04-13 13:25:36 -0500

    added batch scripts to make `pscp` easier

---
+++ changedir.bat
@@ -0,0 +1,2 @@
+
+start cmd /k "cd \dgnava2_maneyko2\SE423Repo\Labs\lab8_dgnava2_maneyko2\lab8_dgnava2_maneyko2Project\Debug"
---
+++ copyover.bat
@@ -0,0 +1,8 @@
+
+:: start cmd /k "cd \dgnava2_maneyko2\SE423Repo\Labs\lab8_dgnava2_maneyko2\lab8_dgnava2_maneyko2Project\Debug"
+@ECHO OFF
+
+SET fname="\dgnava2_maneyko2\SE423Repo\Labs\lab8_dgnava2_maneyko2\lab8_dgnava2_maneyko2Project\Debug\lab8_dgnava2_maneyko2.hex"
+:: start cmd /c "pscp -pw '' \dgnava2_maneyko2\SE423Repo\Labs\lab8_dgnava2_maneyko2\lab8_dgnava2_maneyko2Project\Debug\lab8_dgnava2_maneyko2.hex root@192.168.1.78:"
+pscp -pw '' %fname% root@192.168.1.78:
+PAUSE
